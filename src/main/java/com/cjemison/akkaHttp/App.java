2a6a21e2f2c2562e12bed2a026addc2906e84f65
Cornelius Jemison <cjemison@gogoair.com>
2017-12-01 11:56:02 -0600

    removed static loggers.

--- src/main/java/com/cjemison/akkaHttp/App.java
+++ src/main/java/com/cjemison/akkaHttp/App.java
@@ -1,14 +1,11 @@
 package com.cjemison.akkaHttp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 import akka.NotUsed;
-import akka.actor.AbstractActor;
+import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
@@ -22,30 +19,32 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.japi.pf.ReceiveBuilder;
+import akka.persistence.AbstractPersistentActor;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class App extends AllDirectives {
-  public static final ActorSystem system = ActorSystem.create("routes");
-  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
   private final ActorRef exampleActor;
+  private final MessageDispatcher messageDispatcher;
 
-  public App(final ActorSystem system) {
+  public App(final ActorSystem system, final String dispatcherName) {
     this.exampleActor = system.actorOf(ExampleActor.props());
+    this.messageDispatcher = system.dispatchers().lookup(dispatcherName);
   }
 
   public static void main(String[] args) throws Exception {
+    final ActorSystem system = ActorSystem.create("routes");
     final Http http = Http.get(system);
     final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-    final App app = new App(system);
+    final App app = new App(system, "akka.actor.my-blocking-dispatcher");
 
     final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow
           (system, materializer);
     final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
           ConnectHttp.toHost("localhost", 8080), materializer);
 
-    LOGGER.info("Server online at http://localhost:8080/\nPress RETURN to stop...");
+    System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
     System.in.read(); // let it run until user presses return
 
     binding.thenCompose(ServerBinding::unbind) // trigger unbinding from the port
@@ -53,23 +52,15 @@ public class App extends AllDirectives {
   }
 
   public Route createRoute() {
-    return route(extractRequest(req -> {
-      final MessageDispatcher dispatcher =
-            system.dispatchers().lookup("akka.actor.my-blocking-dispatcher");
-      return completeWithFuture(CompletableFuture.supplyAsync(() -> {
-        exampleActor.tell(UUID.randomUUID().toString(), ActorRef.noSender());
-
-
-        LOGGER.info("Sending Message: temp");
-        return HttpResponse.create()
-              .withStatus(200).withEntity(ContentTypes
-                    .APPLICATION_JSON, "{}");
-      }, dispatcher));
-    }));
+    return route(extractRequest(req -> completeWithFuture(CompletableFuture.supplyAsync(() -> {
+      exampleActor.tell(UUID.randomUUID().toString(), ActorRef.noSender());
+      return HttpResponse.create()
+            .withStatus(200).withEntity(ContentTypes
+                  .APPLICATION_JSON, "{}");
+    }, messageDispatcher))));
   }
 
-  public static class ExampleActor extends AbstractActor {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleActor.class);
+  public static class ExampleActor extends AbstractLoggingActor {
 
     public static Props props() {
       return Props.create(ExampleActor.class);
@@ -82,8 +73,27 @@ public class App extends AllDirectives {
         double count = 0.0;
         while (count < 100000000) {
           count = count + .01;
+          log().info("here");
         }
       }).build();
+    }
+  }
+
+  public static class PersistenceActor extends AbstractPersistentActor {
+
+    @Override
+    public Receive createReceiveRecover() {
+      return null;
+    }
+
+    @Override
+    public Receive createReceive() {
+      return null;
+    }
+
+    @Override
+    public String persistenceId() {
+      return null;
     }
   }
 }
